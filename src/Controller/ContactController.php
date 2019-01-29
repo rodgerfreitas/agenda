23a88b1d071dce12cfdee24ffa822ac5d93b5be6
Rodger Freitas <rodger.freitas@gmail.com>
2019-01-29 18:57:54 -0200

    iniciando o projeto

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,115 @@
+<?php
+  namespace App\Controller;
+
+  use App\Entity\Contact;
+
+  use Symfony\Component\HttpFoundation\Response;
+  use Symfony\Component\HttpFoundation\Request;
+  use Symfony\Component\Routing\Annotation\Route;
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+  use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+  use Symfony\Component\Form\Extension\Core\Type\TextType;
+  use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+  class ContactController extends Controller {
+
+      /**
+       * @Route("/", name="contact_list")
+       * @Method({"GET"})
+       */
+       public function index() {
+          $contacts= $this->getDoctrine()->getRepository(Contact::class)->findAll();
+          return $this->render('contacts/index.html.twig', array('contacts' => $contacts));
+       }
+
+      /**
+       * @Route("/contact/show/{id}", name="contact_show")
+       * @Method({"GET"})
+       */
+      public function show($id) {
+          $contact = $this->getDoctrine()->getRepository(Contact::class)->find($id);
+          return $this->render('contacts/show.html.twig', array('contact' => $contact));
+      }
+
+      /**
+       * @Route("/contact/new", name="contact_new")
+       * Method({"GET", "POST"})
+       */
+      public function new(Request $request) {
+          $contact = new Contact();
+
+          $form = $this->createFormBuilder($contact)
+              ->add('nome', TextType::class, array('attr' => array('class' => 'form-control')))
+              ->add('email', TextType::class, array('attr' => array('class' => 'form-control')))
+              ->add('telefone', TextType::class, array('attr' => array('class' => 'form-control')))
+              ->add('save', SubmitType::class, array(
+                  'label' => 'salvar',
+                  'attr' => array('class' => 'btn btn-primary mt-3')
+              ))
+              ->getForm();
+
+          $form->handleRequest($request);
+
+          if($form->isSubmitted() && $form->isValid()) {
+              $contact = $form->getData();
+
+              $entityManager = $this->getDoctrine()->getManager();
+              $entityManager->persist($contact);
+              $entityManager->flush();
+
+              return $this->redirectToRoute('contact_list');
+          }
+
+          return $this->render('contacts/new.html.twig', array(
+              'form' => $form->createView()
+          ));
+      }
+
+      /**
+       * @Route("/contact/edit/{id}", name="contact_edit")
+       * Method({"GET","POST"})
+       */
+      public function edit(Request $request, $id) {
+
+          $contact = $this->getDoctrine()->getRepository(Contact::class)->find($id) ;
+          $form = $this->createFormBuilder($contact)
+              ->add('nome', TextType::class, array('attr' => array('class' => 'form-control')))
+              ->add('email', TextType::class, array('attr' => array('class' => 'form-control')))
+              ->add('telefone', TextType::class, array('attr' => array('class' => 'form-control')))
+              ->add('save', SubmitType::class, array(
+                  'label' => 'salvar',
+                  'attr' => array('class' => 'btn btn-primary mt-3')
+              ))
+              ->getForm();
+
+          $form->handleRequest($request);
+
+          if($form->isSubmitted() && $form->isValid()) {
+
+              $entityManager = $this->getDoctrine()->getManager();
+              $entityManager->flush();
+
+              return $this->redirectToRoute('contact_list');
+          }
+
+          return $this->render('contacts/edit.html.twig', array(
+              'form' => $form->createView()
+          ));
+      }
+
+      /**
+       * @Route("/contact/delete/{id}")
+       * @Method({"DELETE"})
+       */
+      public function delete(Request $request, $id) {
+          $contact = $this->getDoctrine()->getRepository(Contact::class)->find($id);
+
+          $entityManager = $this->getDoctrine()->getManager();
+          $entityManager->remove($contact);
+          $entityManager->flush();
+
+          $response = new Response();
+          $response->send();
+      }
+  }
---
+++ src/Entity/Contact.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $nome;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $telefone;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $email;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getTelefone(): ?string
+    {
+        return $this->telefone;
+    }
+
+    public function setTelefone(string $telefone): self
+    {
+        $this->telefone = $telefone;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
